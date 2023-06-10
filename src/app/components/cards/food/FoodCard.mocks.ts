f87a1d6f9e7708ab8c77437ee9be515c54ad079b
Quenitn <cailletquentin@hotmail.fr>
2023-06-10 13:03:22 +0200

    feat: utilisation du modele de components

--- src/app/components/cards/food/FoodCard.mocks.ts
+++ src/app/components/cards/food/FoodCard.mocks.ts
@@ -1,7 +1,11 @@
 import { IFoodCard } from './FoodCard';
 
 const base: IFoodCard = {
-  sampleTextProp: 'Hello world!',
+  tag: 'Légume',
+  title: `Appeeling Spuds!`,
+  body: "Notre pomme de terre n'est pas une patate douce, mais elle est bien dans sa peau!",
+  author: 'Quentin',
+  time: '1h ago',
 };
 
 export const mockFoodCardProps = {
--- src/app/components/cards/food/FoodCard.tsx
+++ src/app/components/cards/food/FoodCard.tsx
@@ -1,11 +1,50 @@
+import Image from 'next/image';
 import styles from './FoodCard.module.css';
 
 export interface IFoodCard {
-  sampleTextProp: string;
+  tag: string;
+  title: string;
+  body: string;
+  author: string;
+  time: string;
 }
 
-const FoodCard: React.FC<IFoodCard> = ({ sampleTextProp }) => {
-  return <div className={styles.container}>{sampleTextProp}</div>;
+const FoodCard: React.FC<IFoodCard> = ({ tag, title, body, author, time }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.card__header}>
+          <Image
+            src="/item-food.jpg"
+            alt="card__image"
+            className={styles.card__image}
+            width="400"
+            height="300"
+          />
+        </div>
+        <div className={styles.card__body}>
+          <span className={`${styles.tag} ${styles['tag-blue']}`}>{tag}</span>
+          <h4>{title}</h4>
+          <p>{body}</p>
+        </div>
+        <div className={styles.card__footer}>
+          <div className={styles.user}>
+            <Image
+              src="/avatar.jpg"
+              alt="user__image"
+              className={styles.user__image}
+              width="40"
+              height="40"
+            />
+            <div className={styles.user__info}>
+              <h5>{author}</h5>
+              <small>{time}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
